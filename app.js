61139fa197788d0b1e16f050c3743730547ae91b
Hikmahx <hikmayousuph@gmail.com>
2021-01-22 23:08:44 -0800

    js for click  manual slider completed

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ menu.addEventListener('click', ()=>{
   nav.classList.toggle('open');
   menu.classList.toggle('open');
 })
+
+
+// UI for carousel slider
+const cards = document.querySelector('.cards');
+const  card = document.querySelectorAll('.card');
+const manualBtns = document.querySelectorAll('.manual-btn');
+
+// counter
+let counter = 1;
+const size = card[0].clientWidth + 40;
+
+const radioCards = document.querySelectorAll(`#btn${counter} ~ .first`);
+
+manualBtns.forEach(manualBtn =>{
+  manualBtn.addEventListener('click', (e)=>{
+    let cardNumber = manualBtn.getAttribute('for').charAt(3) -1;
+    manualBtn.checked = true;
+    cards.style.transform = `translateX(-${size * cardNumber}px)`;
+    cards.style.transition = '0.4s ease-in-out';
+  })
+})
